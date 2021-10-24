4ee1a964592e85b13a52be72ab032b4eaddaa248
pmatsa <pmatsaridis@gmail.com>
2021-10-24 15:15:14 +0300

    optimize maps in POIs

--- resources/views/admin/points/show.blade.php
+++ resources/views/admin/points/show.blade.php
@@ -44,7 +44,7 @@
     @parent
 
     <script>
-        var map = L.map('poi-map').setView([38.9386255, 22.2306269], 11);
+        var map = L.map('poi-map').setView([38.9207692, 22.3398289], 11);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
